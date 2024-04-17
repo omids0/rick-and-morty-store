e2ad8780ff6527c0d8652eb8ff8889722074ecad
Omid <soheilnia.o@entekhabelectronic.com>
2024-04-17 11:44:35 +0330

    products cards added to homePage component

--- components/homePage/index.tsx
+++ components/homePage/index.tsx
@@ -1,7 +1,40 @@
+import Image from 'next/image'
 import { type FC } from 'react'
 
+import { productsData } from 'utils/data/product'
+
 const HomePage: FC = () => {
-  return <div>big Hi</div>
+  return (
+    <div className="flex flex-col">
+      <div className="flex justify-between gap-2">
+        {productsData.map(item => (
+          <div className=" bg-red-500 flex flex-col" key={item.id}>
+            <Image
+              src={`/${item.blouse.image}`}
+              alt={item.blouse.name}
+              width={200}
+              height={200}
+              loading="lazy"
+            />
+            <Image
+              src={`/${item.pants.image}`}
+              alt={item.blouse.name}
+              width={200}
+              height={200}
+              loading="lazy"
+            />
+            <Image
+              src={`/${item.shoe.image}`}
+              alt={item.blouse.name}
+              width={200}
+              height={200}
+              loading="lazy"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default HomePage
