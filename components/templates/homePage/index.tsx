16e6b593cf8dfb27a8f750e79827125f347541c8
Omid <soheilnia.o@entekhabelectronic.com>
2024-04-21 11:46:50 +0330

    responsive carousel completed

--- components/templates/homePage/index.tsx
+++ components/templates/homePage/index.tsx
@@ -10,32 +10,35 @@ const HomePage: FC = () => {
   const handleCarouselContent = data => {
     return data.map((item: ProductsData) => ({
       element: (
-        <div className="flex sm:w-[80%] sm:flex-row sm:h-[50vh] h-[70vh] flex-col w-full p-4 sm:justify-between items-center bg-gradient-to-b from-red-50 to-transparent rounded-xl">
+        <div className="flex sm:w-[80%] flex-row sm:h-[50vh] h-[30vh] w-full p-4 justify-between items-center bg-gradient-to-b from-red-50 to-transparent rounded-xl">
           <Image
             src={`/${item.blouse.image}`}
-            className="sm:max-w-[25rem] max-w-[8rem] h-[20rem]"
+            className="sm:max-w-[16rem] max-w-[5rem] sm:max-h-[18rem] max-h-[10rem]"
             width={500}
             height={500}
             alt={item.blouse.name}
+            loading="lazy"
           />
 
           <Image
             src={`/${item.pants.image}`}
-            className="sm:max-w-[12rem] max-w-[8rem]"
+            className="sm:max-w-[12rem] max-w-[6rem] sm:max-h-[16rem] max-h-[10rem]"
             width={500}
             height={500}
             alt={item.pants.name}
+            loading="lazy"
           />
-          <div className="flex sm:flex-col flex-col-reverse items-center sm:justify-between sm:h-full">
-            <h1 className="font-bold sm:text-5xl text-gray-700 opacity-0 sm:opacity-100 first-letter:uppercase">
+          <div className="flex flex-col items-center justify-center h-full gap-5">
+            <h1 className="font-bold sm:text-5xl text-gray-700 first-letter:uppercase">
               {item.name}
             </h1>
             <Image
               src={`/${item.shoe.image}`}
-              className="sm:max-w-[12rem] max-w-[8rem] max-h-[8rem]"
+              className="sm:max-w-[12rem] max-w-[6rem] max-h-[6rem]"
               width={500}
               height={500}
               alt={item.shoe.name}
+              loading="lazy"
             />
           </div>
         </div>
@@ -45,7 +48,7 @@ const HomePage: FC = () => {
 
   return (
     <div className="flex flex-col">
-      <div className="w-full min-h-[50vh] mb-8">
+      <div className="w-full sm:h-[50vh] h-[30vh] mb-8">
         <Carousel contentData={handleCarouselContent(productsData)} />
       </div>
       <div className="flex gap-2 flex-wrap">
